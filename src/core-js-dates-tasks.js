29f1e49e506f3c1cdbda6c65711bad991d6b48ad
Andrei Markouski <insawm@gmail.com>
2024-03-21 21:09:55 +0300

    feat: 9/14

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -167,8 +167,16 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const day = new Date(year, month - 1, 1);
+  let count = 0;
+  while (day.getMonth() === month - 1) {
+    if (day.getDay() === 0 || day.getDay() === 6) {
+      count += 1;
+    }
+    day.setDate(day.getDate() + 1);
+  }
+  return count;
 }
 
 /**
