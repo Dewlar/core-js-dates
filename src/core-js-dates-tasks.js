f4c394af1b97d2d329c358e300f1d452de7fd1f1
Andrei Markouski <insawm@gmail.com>
2024-03-21 20:49:32 +0300

    feat: 8/14 passed

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -123,8 +123,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const start = new Date(period.start).getTime();
+  const end = new Date(period.end).getTime();
+  const currentDate = new Date(date).getTime();
+
+  return start <= currentDate && currentDate <= end;
 }
 
 /**
@@ -138,8 +142,10 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const currentDate = new Date(date);
+  const formattedDate = `${currentDate.getUTCMonth() + 1}/${currentDate.getUTCDate()}/${currentDate.getUTCFullYear()}, ${currentDate.toLocaleTimeString('en-US', { hour12: true, timeZone: 'UTC' })}`;
+  return formattedDate;
 }
 
 /**
